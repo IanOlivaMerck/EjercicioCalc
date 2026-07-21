a76f19874446154d0ca09f4dde987ede69dd0b0e
IanOlivaMerck <ian.oliva@merckgroup.com>
2026-07-21 15:23:34 -0600

    Create Calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,12 @@
+"""
+Module: Calculadora.py
+Purpose: Este módulo es el modulo principal para el uso de funciones como calculadora
+Date: 21JUL26
+Author: I. Oliva
+"""
+
+def run():
+  pass
+
+if __name__ = "__main__":
+  run()
