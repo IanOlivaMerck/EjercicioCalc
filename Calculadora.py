94b74a79a5250d6450af7c57b4a099d171f3d1f6
Ian Oliva <ian.oliva@merckgroup.com>
2026-07-22 10:01:17 -0600

    Se ajustó autor y se elimino string del código

--- Calculadora.py
+++ Calculadora.py
@@ -2,9 +2,9 @@
 Module: Calculadora.py
 Purpose: Este módulo es el modulo principal para el uso de funciones como calculadora
 Date: 21JUL26
-Author: I. Oliva
+Author: I. Oliva / J. Gonzalez
 """
-import string
+
 import Func_SumaResta as SR
 import Func_MultDiv as MD
 
